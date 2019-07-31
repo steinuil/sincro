1be3ace2ce14bea09fed42ad7539d170351ced15
steinuil <steenuil.owl@gmail.com>
2019-07-31 23:21:27 +0200

    use assert.DeepEquals in tests

--- protocol/parse_test.go
+++ protocol/parse_test.go
@@ -1,45 +1,120 @@
 package protocol
 
-import "testing"
+import (
+	"gotest.tools/assert"
+	"testing"
+)
 
 func TestParseHello(t *testing.T) {
 	var data = []byte(`{
-    "username":"test",
-    "room":{"name":"testroom"},
-    "realversion": "1.6.4",
-    "version":"1.2.255",
-    "motd":"testmotd",
-    "features":{
-      "chat":false,
-      "featureList":true,
-      "managedRooms":true,
-      "readiness":true,
-      "sharedPlaylists":false
-    }
-  }`)
+		"username":"test",
+		"room":{"name":"testroom"},
+		"realversion": "1.6.4",
+		"version":"1.2.255",
+		"motd":"testmotd",
+		"features":{
+			"chat":false,
+			"isolateRooms":true,
+			"managedRooms":true,
+			"readiness":true,
+			"maxChatMessageLength":240,
+			"maxUsernameLength":36,
+			"maxRoomNameLength":32,
+			"maxFilenameLength":256
+		}
+	}`)
 
 	hello, err := parseHello(data)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if hello.User != "test" {
-		t.Errorf("hello.User: expected %s, got %s", "test", hello.User)
+	assert.DeepEqual(t, hello, Hello{
+		User:    "test",
+		Room:    "testroom",
+		Version: "1.6.4",
+		Motd:    "testmotd",
+		Features: ServerFeatures{
+			Chat:                 false,
+			IsolateRooms:         true,
+			ManagedRooms:         true,
+			Readiness:            true,
+			MaxChatMessageLength: 240,
+			MaxUsernameLength:    36,
+			MaxRoomNameLength:    32,
+			MaxFilenameLength:    256,
+		},
+	})
+}
+
+func TestParseNewControlledRoom(t *testing.T) {
+	var data = []byte(`{
+		"roomName":"theroom",
+		"password":"swordfish"
+  }`)
+
+	n, err := parseNewControlledRoom(data)
+	if err != nil {
+		t.Error(err)
 	}
 
-	if hello.Room != "testroom" {
-		t.Errorf("hello.Room: expected %s, got %s", "testroom", hello.Room)
+	assert.DeepEqual(t, n, NewControlledRoom{
+		Room:     "theroom",
+		Password: "swordfish",
+	})
+}
+
+func TestParseControllerAuth(t *testing.T) {
+	var data = []byte(`{
+		"user":"me",
+		"room":"this",
+		"success":false
+	}`)
+
+	n, err := parseControllerAuth(data)
+	if err != nil {
+		t.Error(err)
 	}
 
-	if hello.Version != "1.6.4" {
-		t.Errorf("hello.Version: expected %s, got %s", "1.6.4", hello.Version)
+	assert.DeepEqual(t, n, ControllerAuth{
+		User:    "me",
+		Room:    "this",
+		Success: false,
+	})
+}
+
+func TestParseReady(t *testing.T) {
+	var data = []byte(`{
+		"username":"maroka",
+		"isReady":true,
+		"manuallyInitiated":true
+	}`)
+
+	n, err := parseReady(data)
+	if err != nil {
+		t.Error(err)
 	}
 
-	if hello.Motd != "testmotd" {
-		t.Errorf("hello.Motd: expected %s, got %s", "testmotd", hello.Motd)
+	assert.DeepEqual(t, n, Ready{
+		User:                "maroka",
+		IsReady:             true,
+		IsManuallyInitiated: true,
+	})
+}
+
+func TestParsePlaylistIndex(t *testing.T) {
+	var data = []byte(`{
+		"user":"maroka",
+		"index":42
+	}`)
+
+	n, err := parsePlaylistIndex(data)
+	if err != nil {
+		t.Error(err)
 	}
 
-	if hello.Features.Chat != false {
-		t.Errorf("hello.Features.Chat: expected %t, got %t", false, hello.Features.Chat)
-	}
+	assert.DeepEqual(t, n, PlaylistIndex{
+		User:  "maroka",
+		Index: 42,
+	})
 }
